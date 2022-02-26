df167936db1a6b69f940ccd13e264df76790566a
Yongkang He <he@yongkang.cloud>
2022-02-26 16:32:49 +0800

    Create setenv.sh

---
+++ setenv.sh
@@ -0,0 +1,8 @@
+#-------Set the environment variables"
+export MY_CLUSTER=cce4yong1          #Customize your favorite cluster name
+export MY_FLAOVOR_NAME=s3.xlarge.4   #Customize your favorite machine type
+export MY_REGION=ap-southeast-3      #Customize your favorite region
+#export MY_ZONE=us-central1-f        #Customize your favorite zone
+export MY_BUCKET=k10bucket4yong1     #Customize your favorite bucket
+export MY_OBJECT_STORAGE_PROFILE=myobs1 #Customize your favorite profile name
+
