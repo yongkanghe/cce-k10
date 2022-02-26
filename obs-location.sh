57eae8f21c54f4cb633263d257a5d61fcc4e0ec1
Yongkang He <he@yongkang.cloud>
2022-02-26 16:32:41 +0800

    Create obs-location.sh

---
+++ obs-location.sh
@@ -0,0 +1,37 @@
+echo '-------Creating a S3 profile secret'
+. ./setenv.sh
+MY_PREFIX=$(echo $(whoami) | sed -e 's/\_//g' | sed -e 's/\.//g' | awk '{print tolower($0)}')
+
+export AWS_ACCESS_KEY_ID=$(cat hwcaccess | head -1)
+export AWS_SECRET_ACCESS_KEY=$(cat hwcaccess | tail -1)
+
+kubectl create secret generic k10-s3-secret \
+      --namespace kasten-io \
+      --type secrets.kanister.io/aws \
+      --from-literal=aws_access_key_id=$AWS_ACCESS_KEY_ID \
+      --from-literal=aws_secret_access_key=$AWS_SECRET_ACCESS_KEY
+
+echo '-------Creating an OSS S3 profile'
+cat <<EOF | kubectl apply -f -
+apiVersion: config.kio.kasten.io/v1alpha1
+kind: Profile
+metadata:
+  name: $MY_OBJECT_STORAGE_PROFILE
+  namespace: kasten-io
+spec:
+  type: Location
+  locationSpec:
+    credential:
+      secretType: AwsAccessKey
+      secret:
+        apiVersion: v1
+        kind: Secret
+        name: k10-s3-secret
+        namespace: kasten-io
+    type: ObjectStore
+    objectStore:
+      name: $MY_PREFIX-$MY_BUCKET
+      objectStoreType: S3
+      region: $MY_REGION
+      endpoint: obs.$MY_REGION.myhuaweicloud.com
+EOF
